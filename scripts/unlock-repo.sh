23edad0945ef0dc1db126f5ef1510c712492a221
John Lindholm <johnl@axis.com>
2026-03-31 09:13:08 +0200

    fix unlock script

--- scripts/unlock-repo.sh
+++ scripts/unlock-repo.sh
@@ -5,7 +5,7 @@ SECRET_NAME="gitcrypt"
 KEY_NAME="$SECRET_NAME-gitcrypt.key"
 
 echo "$SCRIPT_NAME: Unlocking repo with key $SECRET_NAME"
-KEY_BASE64=$(aws secretsmanager get-secret-value --secret-id "$KEY_NAME" | jq -r '.SecretBinary')
+KEY_BASE64=$(aws secretsmanager get-secret-value --secret-id "$SECRET_NAME" | jq -r '.SecretBinary')
 echo "$KEY_BASE64" | base64 --decode > "$KEY_NAME"
 git-crypt unlock "$KEY_NAME"
 rm "$KEY_NAME"
